e87c7e65e220d9f2f4e6f8b782dc3df79611b128
hayss007@gmail.com <hayss007@gmail.com>
2026-06-11 13:57:16 +0200

    Initial Commit

---
+++ Habbits/HabbitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitsApp.swift
+//  Habbits
+//
+//  Created by Przemek Hussar on 11/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HabbitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
